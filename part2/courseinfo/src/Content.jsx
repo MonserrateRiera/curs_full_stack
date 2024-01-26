ebb92423e11a8ceaa0ac8d2b5b69310333103f77
mrieramar <mrieramar@uoc.edu>
2024-01-26 10:36:08 +0100

    Updated to part2 exercises

--- part2/courseinfo/src/Content.jsx
+++ part2/courseinfo/src/Content.jsx
@@ -1,10 +1,12 @@
 import Part from "./Part";
 import Header from "./Header";
 import Total from "./Total";
-const Content = ({course}) =>{
 
+const Content = ({course}) =>{
+  
     return (
         <div>
+            
            <Header course = {course.name} />
            {course.parts.map(part => 
                 <Part part = {part} key={part.id}/>
