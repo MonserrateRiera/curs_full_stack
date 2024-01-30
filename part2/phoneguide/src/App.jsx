fa9d3afdbad0494556839d8b16251d23db63e182
mrieramar <mrieramar@uoc.edu>
2024-01-30 10:29:20 +0100

    Added dinamic styles

--- part2/phoneguide/src/App.jsx
+++ part2/phoneguide/src/App.jsx
@@ -3,14 +3,14 @@ import Form from './Form'
 import PersonList from './PersonList'
 import Filter from './Filter'
 import Services from './Services/Services'
-
+import Notification from './Notification'
 
 //TODO add styles
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  
-
+  const [persons, setPersons] = useState([]);
+  const [message, setMessage] = useState(null);
+  const [clasname, setClasname] = useState('');
   useEffect(() => {
     console.log('effect')
     const data = Services.getAll();
@@ -23,6 +23,8 @@ const App = () => {
     if (isValid(newPerson)){
       Services.postNew(newPerson)
       .then(data => setPersons(persons.concat(data)))
+      setMessage("user created correctly");
+      setClasname('notification');
     }
     else{
       confirmUpdate(newPerson);
@@ -43,9 +45,18 @@ const App = () => {
 
   const updatePerson = (newPerson) =>{
     const id = persons.find(person => person.name === newPerson.name).id;
-    Services.updatePerson(id, newPerson)
-    .then(data => setPersons(persons.map(person => person.id !== data.id ? person : data)))
-    console.log(`updating ${newPerson.name} with number ${newPerson.number}`)
+    Services
+    .updatePerson(id, newPerson)
+    .then(data => {
+      setPersons(persons.map(person => person.id !== data.id ? person : data))
+      setMessage("User edited correctly")
+    })
+    .catch(error =>{
+      setMessage("There's an Error!");
+      setClasname('error');
+    })
+    
+    
   }
 
   const filterPersons = () =>{
@@ -75,6 +86,7 @@ const App = () => {
       <h2>Phonebook</h2>
       <Filter onChange={setfilter}/>
       <Form onSubmit={addPerson}  />
+      <Notification message={message} clasname = {clasname} />
       <h2>Numbers</h2>
       <PersonList persons={filterPersons()} onDelete={deletePerson}/>
     </div>
