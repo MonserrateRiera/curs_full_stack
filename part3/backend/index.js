7f4426f6880f7212ad35ebe4ca114818a3258fe0
mrieramar <mrieramar@uoc.edu>
2024-02-01 11:06:07 +0100

    added post method

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,32 +1,33 @@
 const express = require('express');
 const app = express();
+app.use(express.json())
 
 let agenda = [
     
           {
             "name": "Arto Hellas",
             "number": "040-123456",
-            "id": "1"
+            "id": 1
           },
           {
             "name": "Ada Lovelace",
             "number": "39-44-5323523",
-            "id": "2"
+            "id": 2
           },
           {
             "name": "Dan Abramov",
             "number": "12-43-234345",
-            "id": "3"
+            "id": 3
           },
           {
             "name": "Mary Poppendieck",
             "number": "39-23-6423122",
-            "id": "4"
+            "id": 4
           },
           {
             "name": "Arto Hellas Number",
             "number": "11111",
-            "id": "1fdb"
+            "id": 5
           }
 ];
 
@@ -44,7 +45,7 @@ app.get('/', (request, response) => {
   })
 
   app.get('/api/persons/:id',(request, response)=>{
-    const id = request.params.id;
+    const id = Number(request.params.id);
     const person = agenda.find(person=> person.id === id);
     if(person){
         response.json(person);
@@ -52,6 +53,54 @@ app.get('/', (request, response) => {
         response.status(404).end()
     }
   })
+
+  app.delete('/api/persons/:id', (request, response) =>{
+    
+    const id = Number(request.params.id);
+    agenda = agenda.filter(person => person.id !== id);
+    response.status(204).end();
+  })
+
+  app.post('/api/persons', (request, response) =>{
+    console.log("lo que reb per es request: ", request.body );
+    const {name, number} = request.body;
+    const id = generateId();
+    const newEntry = {
+      name,
+      number,
+      id
+    };
+    console.log("prova amb vainas de js", newEntry);
+    if(isEmpty(request.body.name)&& isEmpty(request.body.number)){
+      return response.status(400).json({ error: 'name and phone cant be empty' });
+    }
+    if(isRepeated(name)){
+      return response.status(400).json({ error: 'Name cant be repeatded' });
+    }
+    
+      agenda.push(newEntry);
+      response.status(200).json(newEntry);
+      console.log(agenda);
+  }) 
+
+  const generateId = () => {
+    const min = agenda.length;
+    const max = 1000;
+    return Math.floor(Math.random() * (max-min)+min);
+  }
+  
+  const isEmpty = (parameter) =>{
+    if(!parameter || parameter.trim() === ''){
+      return true;
+    }
+    return false;
+  }
+  const isRepeated = (name) => {
+    if(agenda.find(entry => entry.name === name)){
+      return true;
+    }
+    return false;
+  };
 
   const PORT = 3001
   app.listen(PORT, () => {
