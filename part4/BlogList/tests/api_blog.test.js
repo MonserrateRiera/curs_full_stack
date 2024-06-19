7cb88a36f8426803c558d1b93ab9899c1c72c6ef
Monserrate Riera <mrieramar@uoc.edu>
2024-06-19 18:49:26 +0200

    chore: Update configuration for test environment variables and add teardown script

---
+++ part4/BlogList/tests/api_blog.test.js
@@ -0,0 +1,55 @@
+
+//Importam els moduls per fer els tests
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const blog = require('../models/blog');
+
+//Inicialitzam el supertest amb l'aplicació express.
+const api = supertest(app);
+
+//Deixam la base de dades en un estat conegut abans de fer els tests.
+const inicialBlogs = [
+    {
+        title: 'Blog testing 1',
+        author: 'Monserrat',
+        url: 'testing1Monxo.com',
+        likes: 4
+    },
+    {
+        title: 'Blog testing 2',
+        author: 'Tomas',
+        url: 'testing2Tomas.com',
+        likes: 4
+    },
+    {
+        title: 'Blog testing 3',
+        author: 'cati',
+        url: 'testing3cati.com',
+        likes: 4
+    }
+];
+beforeEach(async () =>{
+    //eliminam tots els blogs i insertam els tres que he creat abans a la base de dades de proves.
+    await blog.deleteMany({});
+    let blogObject = new blog(inicialBlogs[0]);
+    await blogObject.save();
+    blogObject = new blog(inicialBlogs[1]);
+    await blogObject.save();
+    blogObject = new blog(inicialBlogs[2]);
+    await blogObject.save();
+})
+
+describe('Group of tests trying the api', () => {
+
+    test('should return the number of blogs from the api',  async () => {
+        const response = await api.get('/api/blogs/');
+        console.log('response of the database ',response.body);
+        expect(response.body).toHaveLength(inicialBlogs.length);
+    });
+});
+
+
+afterAll(() => {
+    mongoose.connection.close()
+  })
--- part4/BlogList/utils/config.js
+++ part4/BlogList/utils/config.js
@@ -1,8 +1,8 @@
 const dotenv = require('dotenv').config();
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
-
+//si l'executam amb test, agafam sa variable de test.
+const MONGODB_URI = process.env.NODE_ENV === 'test'   ? process.env.TEST_MONGODB_URI  : process.env.MONGODB_URI
 module.exports = {
   MONGODB_URI,
   PORT
