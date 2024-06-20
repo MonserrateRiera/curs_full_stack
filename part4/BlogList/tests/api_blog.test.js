3bd89284c43338c22ca8a352c954c81b50752ab1
Monserrate Riera <mrieramar@uoc.edu>
2024-06-20 14:41:40 +0200

    feat: Refactor test setup and data initialization

--- part4/BlogList/tests/api_blog.test.js
+++ part4/BlogList/tests/api_blog.test.js
@@ -4,39 +4,21 @@ const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
 const blog = require('../models/blog');
+const helper = require('./test_helper');
 
 //Inicialitzam el supertest amb l'aplicació express.
 const api = supertest(app);
 
 //Deixam la base de dades en un estat conegut abans de fer els tests.
-const inicialBlogs = [
-    {
-        title: 'Blog testing 1',
-        author: 'Monserrat',
-        url: 'testing1Monxo.com',
-        likes: 4
-    },
-    {
-        title: 'Blog testing 2',
-        author: 'Tomas',
-        url: 'testing2Tomas.com',
-        likes: 4
-    },
-    {
-        title: 'Blog testing 3',
-        author: 'cati',
-        url: 'testing3cati.com',
-        likes: 4
-    }
-];
+
 beforeEach(async () =>{
     //eliminam tots els blogs i insertam els tres que he creat abans a la base de dades de proves.
     await blog.deleteMany({});
-    let blogObject = new blog(inicialBlogs[0]);
+    let blogObject = new blog(helper.inicialBlogs[0]);
     await blogObject.save();
-    blogObject = new blog(inicialBlogs[1]);
+    blogObject = new blog(helper.inicialBlogs[1]);
     await blogObject.save();
-    blogObject = new blog(inicialBlogs[2]);
+    blogObject = new blog(helper.inicialBlogs[2]);
     await blogObject.save();
 })
 
@@ -45,7 +27,7 @@ describe('API test, getting blogs', () => {
     test('should return the number of blogs from the api',  async () => {
         const response = await api.get('/api/blogs/');
         console.log('response of the database ',response.body);
-        expect(response.body).toHaveLength(inicialBlogs.length);
+        expect(response.body).toHaveLength(helper.inicialBlogs.length);
     });
     test('should return the id like id and not _id', async () => {
         const response = await api.get('/api/blogs/');
---
+++ part4/BlogList/tests/test_helper.js
@@ -0,0 +1,28 @@
+const blog = require ('../models/blog');
+
+const inicialBlogs = [
+    {
+        title: 'Blog testing 1',
+        author: 'Monserrat',
+        url: 'testing1Monxo.com',
+        likes: 4
+    },
+    {
+        title: 'Blog testing 2',
+        author: 'Tomas',
+        url: 'testing2Tomas.com',
+        likes: 4
+    },
+    {
+        title: 'Blog testing 3',
+        author: 'cati',
+        url: 'testing3cati.com',
+        likes: 4
+    }
+];
+
+
+
+module.exports = {
+    incialBlogs
+}
